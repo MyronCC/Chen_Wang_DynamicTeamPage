b7e3f2728845a50d89d48a3fdf21df3331107ce5
MyronCC <b_chen9@fanshaweonline.com>
2019-03-17 02:40:45 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,17 +1,42 @@
 (() => {
  	console.log('fire');
 
-	var name = ['Chen Boling', 'Wang Jianghaonan'];
-	var photo = document.querySelectorAll('.photo');
- 
- 	photo.onclick = function() {
- 		alert(name[1]);
- 	}
+	let name = ["Chen Boling", "Wang Jianghaonan"];
+		job = ["Developer", "Designer"];
 
- 	photo.addEventListener('click', function(){
- 		alert(name[2]);
- 	}
- 	false);
+		chen = document.getElementsByClassName('developer');
+		wang = document.getElementsByClassName('designer');
 
- 	var img = document.querySelectorAll
+		overlay = document.getElementById('overlay');
+
+		overlayName = document.getElementById('name');
+		overlayJob = document.getElementById('job');
+
+		for (let i = 0; i < chen.length; i++) {
+			chen[i].addEventListener('click', function () {
+
+			overlay.classList.remove('hide'); 
+
+			overlayName.textContent = name[0];
+			overlayJob.textContent = job[0];
+			}, false)
+		}
+
+		for (let i = 0; i < wang.length; i++) {
+			wang[i].addEventListener('click', function () {
+
+			overlay.classList.remove('hide'); 
+
+			overlayName.textContent = name[1];
+			overlayJob.textContent = job[1];
+			}, false)
+		}
+
+		let overlayClose = document.getElementById('overlayClose');
+	
+			overlayClose.addEventListener('click', function() {
+
+			overlay.classList.add('hide');
+		}, false);
+	
 })();
