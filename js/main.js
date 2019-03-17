a3c46763ebf43168e4544e40850b70c5d9e85520
MyronCC <b_chen9@fanshaweonline.com>
2019-03-16 22:19:18 -0400

    update

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(() => {
+ 	console.log('fire');
+
+	var name = ['Chen Boling', 'Wang Jianghaonan'];
+	var photo = document.querySelectorAll('.photo');
+ 
+ 	photo.onclick = function() {
+ 		alert(name[1]);
+ 	}
+
+ 	photo.addEventListener('click', function(){
+ 		alert(name[2]);
+ 	}
+ 	false);
+
+ 	var img = document.querySelectorAll
+})();
